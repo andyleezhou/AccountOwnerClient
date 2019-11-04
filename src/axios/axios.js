187f6020b194ef4e310c5b4694da43b92bc62a37
Andy Zhou <andy.lee.zhou@gmail.com>
2019-11-04 13:49:13 -0800

    Update Axios

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://pacific-meadow-89341.herokuapp.com',
     headers: {
         headerType: 'example header type'
     }
